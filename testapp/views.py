764e4b4489c37532fbc1a8796ee3d9d0b1254b82
sklearn21 <sklearn21@gmail.com>
2021-03-18 10:05:55 +0530

    Added modelbased View.

---
+++ testapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from testapp.models import Book
+
+# Create your views here.
+
+
+# def info_view(request):
+#     books = Book.objects.all()
+#     return render(request, 'testapp/info.html', {'books': books})
+
+
+class BookListView(ListView):
+    model = Book
+    # default template: book_list.html
+    # default context object: book_list
